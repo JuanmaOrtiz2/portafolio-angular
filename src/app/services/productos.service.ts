2273532f9b8ca24f4ff5cc164f3337639329bc31
Juan Manuel Ortiz <juanmaortga@gmail.com>
2020-09-03 20:07:12 +0200

    No funciona el buscador pero esta implementado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,22 +10,59 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-546f1.firebaseio.com/productos_idx.json')
-    .subscribe((resp:Producto[])=>{
-      console.log(resp);
-      this.productos = resp;
 
-      setTimeout(()=>{
-        this.cargando=false;
-      },2000);
-
-      
+    return new Promise((resolve,reject)=>{
+      this.http.get('https://angular-html-546f1.firebaseio.com/productos_idx.json')
+      .subscribe((resp:Producto[])=>{
+        console.log(resp);
+        this.productos = resp;
+  
+        setTimeout(()=>{
+          this.cargando=false;
+        },2000);
+        resolve();
+        
+      });
     });
+
+
+
+    
   }
+
+  getProducto(id:string){
+    return this.http.get(`https://angular-html-546f1.firebaseio.com/productos/${id}.json`)
+  }
+
+  buscarProducto(termino:string)
+  {
+    if(this.productos.length === 0){
+      this.cargarProductos().then(()=>{
+        this.filtrarProductos(termino);
+      });
+    }else{
+      this.filtrarProductos(termino);
+    }
+    this.productosFiltrado= this.productos.filter(producto => {
+      return true;
+    })
+  }
+
+  private filtrarProductos(termino: string){
+    
+    this.productosFiltrado = [];
+    this.productos.forEach(prod => {
+      if(prod.categoria.indexOf(termino)>=0 || prod.titulo.indexOf(termino) >= 0){
+        this.productosFiltrado.push(prod);
+      }
+    })
+  }
+
 }
